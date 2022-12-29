798b8c6c5809ed7da226bafe9c44352a6c14a6d6
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:46:03 +0530

    Committing files

---
+++ CodeChef/C++14/CREDS/72880416.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tt;
+	cin>>tt;
+	
+	int xx,yy,zz;
+	while(tt--)
+	{
+	  cin>>xx>>yy>>zz;
+    cout<<((xx*4)+(yy*2))<<endl;
+	  
+	}
+	
+
+	return 0;
+}
