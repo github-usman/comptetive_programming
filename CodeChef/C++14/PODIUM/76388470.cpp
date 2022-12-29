7b2ca296d1627090190ff7c4fc48a7ca403f832e
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:30:48 +0530

    Committing files

---
+++ CodeChef/C++14/PODIUM/76388470.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+void solver(){
+  int aa,bb;
+  cin>>aa>>bb;
+  
+  cout<<aa+bb<<endl;
+}
+
+int main() {
+  
+  int tt;
+  cin>>tt;
+  
+  while(tt--)
+  solver();
+	
+	return 0;
+}
