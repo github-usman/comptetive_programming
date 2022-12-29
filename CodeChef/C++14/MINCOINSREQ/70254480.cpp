17339e76d753f6d4c04eea46119ee65b554ccc59
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:47:53 +0530

    Committing files

---
+++ CodeChef/C++14/MINCOINSREQ/70254480.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+////////////***First Approach***////////////
+void driver(){
+
+    ll n;
+    cin>>n;
+
+    // ll notes = n/10;
+    ll coints = n%10;
+    cout<<coints<<endl;
+}
+
+//////////////***Main Function***//////////////
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    driver();
+
+
+
+    return 0;
+}
