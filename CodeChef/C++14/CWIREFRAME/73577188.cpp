a66c014304de2bd5ee0afd01fa04843d51ec6268
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:33:26 +0530

    Committing files

---
+++ CodeChef/C++14/CWIREFRAME/73577188.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long int
+
+void solve(){
+  ll nn,mm,xx;
+  cin>>nn>>mm>>xx;
+  
+  cout<<2*(nn+mm)*xx<<endl;
+}
+
+int main() {
+	 int t;
+	 cin>>t;
+	 while(t--)
+	 solve();
+	return 0;
+}
