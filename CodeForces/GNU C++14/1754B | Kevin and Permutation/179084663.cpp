393cab3312eca3f1d2df4e0057609ea86654c7c9
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:51:44 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1754B | Kevin and Permutation/179084663.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+ 
+////////////***First Approach***////////////
+void driver()
+{
+ 
+    ll nn;
+    cin >> nn;
+ 
+    // vector<ll> even, Odd;
+ 
+    if (nn < 4)
+    {
+        for (int i = 0; i < nn; i++)
+        {
+            cout << i + 1 << " ";
+        }
+        cout << endl;
+        return;
+    }
+ 
+    ll mid = (nn / 2)+1;
+    ll st = 1;
+ 
+    if (nn & 1)
+    {
+        for (int i = 0; i < nn-1; i++)
+        {
+            if (i & 1)
+            {
+                cout << st << " ";
+                st += 1;
+            }
+            else
+            {
+ 
+                cout << mid << " ";
+                mid += 1;
+            }
+        }
+        cout << nn << " ";
+        cout << endl;
+    }
+    else
+    {
+        for (int i = 0; i < nn; i++)
+        {
+            if (i & 1)
+            {
+                cout << st << " ";
+                st += 1;
+            }
+            else
+            {
+ 
+                cout << mid << " ";
+                mid += 1;
+            }
+        }
+        cout << endl;
+    }
+}
+ 
+//////////////***Main Function***//////////////
+int32_t main()
+{
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+        driver();
+ 
+    return 0;
+}
