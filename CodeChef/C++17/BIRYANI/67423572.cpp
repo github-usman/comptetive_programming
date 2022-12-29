9874c5917de79f03c1f9cef3abe3d67caca4c3e3
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:49:40 +0530

    Committing files

---
+++ CodeChef/C++17/BIRYANI/67423572.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+////////////***First Approach***////////////
+void driver(){
+	ll x,y;
+	cin>>x>>y;
+	cout<<x*y<<endl;
+	
+}
+
+//////////////***Main Function***//////////////
+int32_t main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	cin>>t;
+
+	while(t--)
+	driver();
+
+
+
+	return 0;
+}
