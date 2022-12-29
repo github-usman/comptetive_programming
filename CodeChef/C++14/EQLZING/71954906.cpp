2c60a5dcb38a8c5d3d1087a383c258f133f37300
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:36:25 +0530

    Committing files

---
+++ CodeChef/C++14/EQLZING/71954906.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+////////////***First Approach***////////////
+void driver(){
+
+    
+    ll a,b;
+    cin>>a>>b;
+
+    if(a == b){
+        cout<<"YES"<<endl;
+        return;
+    }
+
+    if((a-b)%2 == 0){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+}
+
+//////////////***Main Function***//////////////
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    driver();
+
+
+
+    return 0;
+}
