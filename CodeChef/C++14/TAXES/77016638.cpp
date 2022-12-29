1cf9237a6bed968d1a119b80be31bec05feadd33
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:45:02 +0530

    Committing files

---
+++ CodeChef/C++14/TAXES/77016638.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	  int x;
+	  cin>>x;
+	  
+	  (x>100)?cout<<x-10<<endl:cout<<x<<endl;
+	}
+	return 0;
+}
