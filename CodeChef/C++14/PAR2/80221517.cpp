569a4215d2fa6f5c39eb99bad5bf934362cd9148
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:28:22 +0530

    Committing files

---
+++ CodeChef/C++14/PAR2/80221517.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void solve(){
+  int n;
+  cin>>n;
+  
+  if(n%2 ==0){
+    cout<<"YES"<<endl;
+  }else{
+    cout<<"NO"<<endl;
+  }
+}
+
+int main() {
+	// your code goes here
+	
+	
+	
+	int tt;
+	cin>>tt;
+	
+	while(tt--){
+	  solve();
+	}
+	return 0;
+}
