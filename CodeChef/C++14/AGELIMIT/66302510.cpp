b28ca1f6ffc47f6df216d4f3ea90ab7b6d53bbe5
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:43:49 +0530

    Committing files

---
+++ CodeChef/C++14/AGELIMIT/66302510.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+////////////***First Approach***////////////
+void driver(){
+
+    ll x,y,a;
+    cin>>x>>y>>a;
+
+    if(a>=x && a<y){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    
+}
+
+//////////////***Main Function***//////////////
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    driver();
+
+
+
+    return 0;
+}
