dec87ee13d33dd8bda061034f3554097311b213a
Usman Ali Ansari <84284941+github-usman@users.noreply.github.com>
2022-12-29 13:47:58 +0530

    Committing files

---
+++ CodeChef/C++14/CHESSTIME/70254425.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+////////////***First Approach***////////////
+void driver(){
+
+    ll n;
+    cin>>n;
+    cout<<(n*60)/20<<endl;
+}
+
+//////////////***Main Function***//////////////
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    driver();
+
+
+
+    return 0;
+}
